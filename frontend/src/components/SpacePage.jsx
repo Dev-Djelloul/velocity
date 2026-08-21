f5a1118a87eb5855adfab6b1d423f60bbd50c62a
Djelloul Abid <digitalblueskye@gmail.com>
2026-08-21 17:41:28 +0200

    Revert "Remet l'étoile favori à côté du nom du plan sur mobile"
    This reverts commit c9de2711fbfd258dde898301a8442b4a0cc374f2.

--- frontend/src/components/SpacePage.jsx
+++ frontend/src/components/SpacePage.jsx
@@ -259,14 +259,12 @@ export default function SpacePage({ lang, onBack, onLoadPlan, onLoadDraft, onCre
                 >
                   {p.isFavorite ? '⭐' : '☆'}
                 </button>
-                <div className="account-list-item-actions">
-                  <button className="account-list-item-move" onClick={() => handleDuplicate(p)} title={t(lang, 'plans.duplicate')}>
-                    <IconCopy width={14} height={14} />
-                  </button>
-                  <button className="account-list-item-delete" onClick={() => setDeleteTarget(p)} title="Delete">
-                    <IconTrash width={14} height={14} />
-                  </button>
-                </div>
+                <button className="account-list-item-move" onClick={() => handleDuplicate(p)} title={t(lang, 'plans.duplicate')}>
+                  <IconCopy width={14} height={14} />
+                </button>
+                <button className="account-list-item-delete" onClick={() => setDeleteTarget(p)} title="Delete">
+                  <IconTrash width={14} height={14} />
+                </button>
               </div>
             ))}
           </div>
